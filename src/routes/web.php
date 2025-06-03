4466fe135b3c490a35bc4b991c54352451bb46fe
misha366 <misha.moskalenko.jv@gmail.com>
2025-06-03 20:09:40 +0300

    added fortify to the project

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,12 +7,19 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
 
 Route::get('/', fn () => Inertia::render('Main'));
+// Route::post('/contact', [ContactController::class, 'contact']);
+
 // temp
 Route::get('/about', fn () => Inertia::render('About'));
 
 // Authorization - потом обработать с фортифаем
-Route::get('/login', fn () => Inertia::render('Login'))->name('login');
-Route::get('/register', fn () => Inertia::render('Register'))->name('register');
+// Route::get('/login', fn () => Inertia::render('Login'))->name('login');
+// Route::get('/register', fn () => Inertia::render('Register'))->name('register');
+Route::middleware(['auth'])->group(function () {
+    // Добавить контроллер
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
+    // Route::put('/user/profile-information', [ProfileController::class, 'update'])->profile('profile.update');
+});
 
 // Catalog
 Route::get('/catalog', [ProductController::class, 'index'])->name('catalog');
@@ -22,7 +29,7 @@ Route::get('/catalog/{product}', [ProductController::class, 'show'])->name('sing
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
 Route::post('/cart/remove/{product}', [CartController::class, 'remove'])->name('cart.remove');
-Route::post('/cart/update/{product}', [CartController::class, 'update'])->name('cart.update');
+Route::put('/cart/update/{product}', [CartController::class, 'update'])->name('cart.update');
 Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 
 // Checkout
